ea8d02fb20c212ad77aec1947061f3c0eb2522b7
brian901106 <61595880+brian901106@users.noreply.github.com>
2023-03-13 04:05:58 +0800

    add claws

--- Resources/claw/1/rotate_new.py
+++ Resources/claw/1/rotate_new.py
@@ -11,14 +11,31 @@ def rotate_img(img,angle):
     M = np.float32([[1, 0, 476], [0, 1, 0]])
 
     #img = cv2.resize(img)
-    img2 = cv2.warpAffine(img, M, (1100, 800))
+    full_img = cv2.warpAffine(img, M, (1100, 800))
 
     
     center = (550,85)
-    M2 = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotate_img = cv2.warpAffine(img2, M2, (1100, 800))
+
     
-    return rotate_img
+    M2 = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotate_img = cv2.warpAffine(full_img, M2, (1100, 800))
+    
+    scale = 1.5
+    center = (550*scale,85*scale)
+    resize_img = cv2.resize(rotate_img,(0,0),fx=scale,fy=scale,interpolation=cv2.INTER_AREA)
+
+    # 裁切區域的 x 與 y 座標（左上角）
+    x = int(550*(scale-1))
+    y = int(85*(scale-1))
+
+    # 裁切區域的長度與寬度
+    w = 1100
+    h = 800
+
+    # 裁切圖片
+    crop_img = resize_img[y:y+h, x:x+w]
+
+    return crop_img
 
 
 if __name__ == "__main__":
@@ -34,32 +51,10 @@ if __name__ == "__main__":
             img = cv2.imread(filepath)
 
 
-            num = 0
-
-            for angle in range(70,5,-5):
-
-                img_rotated = rotate_img(img, -angle)
-
-                if not os.path.isdir(dir_path+"/rotate/"):
-                    os.mkdir(dir_path+"/rotate/")
-                if not os.path.isdir(dir_path+"/rotate/"+str(-angle)):
-                    os.mkdir(dir_path+"/rotate/"+str(-angle))
-
-                newfilepath = dir_path + "/rotate/" + str(-angle) + "/" + file
-                cv2.imwrite(newfilepath, img_rotated)
-
-            angle = 0
-            img_rotated = rotate_img(img, angle)
-
-            if not os.path.isdir(dir_path+"/rotate/"):
-                os.mkdir(dir_path+"/rotate/")
-            if not os.path.isdir(dir_path+"/rotate/"+str(angle)):
-                os.mkdir(dir_path+"/rotate/"+str(angle))
-
-            newfilepath = dir_path + "/rotate/" + str(angle) + "/" + file
-            cv2.imwrite(newfilepath, img_rotated)
+            angles_a_round = [70, 69, 67, 64, 60, 56, 50, 44, 36, 28, 20, 15, 8, 2, -5, -14, -23, -30, -36, -42, -48, -53, -57, -61, -65, -67, -68, -68, -68, -67, -66, -63, -59, -54, -48, -42, -35, -28, -20, -12, -5, 2, 10, 17, 25, 33, 39, 46, 52, 57, 61, 64, 67, 69, 70]
             
-            for angle in range(5,71,5):
+            num = 1
+            for angle in angles_a_round:
 
                 img_rotated = rotate_img(img, angle)
 
@@ -70,6 +65,7 @@ if __name__ == "__main__":
 
                 newfilepath = dir_path + "/rotate/" + str(angle) + "/" + file
                 cv2.imwrite(newfilepath, img_rotated)
+                num = num + 1
             
             
 

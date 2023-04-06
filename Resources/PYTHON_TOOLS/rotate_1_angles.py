87048380a5948dca14da9823df5fcb4865bec587
brian901106 <61595880+brian901106@users.noreply.github.com>
2023-04-06 22:25:50 +0800

    python

---
+++ Resources/PYTHON_TOOLS/rotate_1_angles.py
@@ -0,0 +1,106 @@
+"""
+ VPython教學: 9-1.單擺
+ Ver. 1: 2018/2/25
+ Ver. 2: 2019/9/8
+ 作者: 王一哲
+ https://hackmd.io/@yizhewang/H1ArzImf7?type=view
+"""
+from vpython import *
+import time
+
+"""
+ 1. 參數設定, 設定變數及初始值
+"""
+m = 1               # 小球質量
+size = 0.2          # 小球半徑
+L = 5               # 擺長
+theta0 = radians(70)# 起始擺角, 用 radians 將單位換成 rad
+theta = theta0      # 擺角
+g = 9.8             # 重力加速度
+T = 2*pi*sqrt(L/g)  # 單擺週期理論值, L=5, g=9.8, T=4.48798950512828
+alpha = 0           # 角加速度, 初始值為 0
+omega = 0           # 角速度, 初始值為 0
+i = 0               # 小球經過週期次數
+t = 0               # 時間
+dt = 0.001          # 時間間隔
+
+last_time = time.time()     # 最後一次取樣的時間
+degree_list = []
+"""
+ 2. 畫面設定
+"""
+# 產生動畫視窗、天花板、小球、繩子
+scene = canvas(title="Pendulum", width=600, height=600, x=0, y=0, background=vec(0, 0.6, 0.6))
+roof = box(pos=vec(0, L/2 + 0.05, 0), size=vec(L, 0.1, 0.5*L), color=color.blue)
+ball = sphere(pos=vec(L*sin(theta0), L/2 - L*cos(theta0), 0), radius=size, color=color.red,
+              make_trail=True, retain=100, v=vec(0, 0, 0))
+rope = cylinder(pos=vec(0, L/2, 0), axis=ball.pos - vec(0, L/2, 0), radius=0.1*size, color=color.yellow)
+# 產生表示速度的箭頭
+arrow_v = arrow(pos=ball.pos, axis=vec(0, 0, 0), shaftwidth=0.3*size, color=color.green)
+arrow_vx = arrow(pos=ball.pos, axis=vec(0, 0, 0), shaftwidth=0.3*size, color=color.magenta)
+arrow_vy = arrow(pos=ball.pos, axis=vec(0, 0, 0), shaftwidth=0.3*size, color=color.orange)
+# 繪圖部分
+gd = graph(title="plot", width=600, height=450, x=0, y=600, xtitle="<i>t</i> (s)", 
+           ytitle="blue: theta (rad), green: omega (rad/s), red: alpha (rad/s<sup>2</sup>)")
+theta_t = gcurve(graph=gd, color=color.blue)
+omega_t = gcurve(graph=gd, color=color.green)
+alpha_t = gcurve(graph=gd, color=color.red)
+"""
+ 2.5 sampling
+"""
+def sampling(now_time,last_time):
+
+    if (now_time - last_time > 0.08):
+        degree = theta*(180/pi)
+        # print("角度:",degree)
+        store_to_list(degree+1)
+        last_time = time.time()
+    return last_time
+
+def store_to_list(degree):
+    degree_list.append(int(degree))
+"""
+ 3. 物體運動部分, 重覆5個週期
+"""
+omega_p = omega
+
+while(i < 1):
+    rate(1000)
+# 計算小球所受力矩、角加速度、角速度、擺角
+    r = ball.pos - vec(0, L/2, 0)
+    F = vec(0, -m*g, 0)
+    torque = cross(r, F)
+    alpha = torque.z/(m*L*L)
+    #alpha = -m*g*ball.pos.x/(m*L*L)
+    omega += alpha*dt
+    theta += omega*dt
+# 更新小球的位置、速度, 繩子的軸方向及長度
+    ball.pos = vec(L*sin(theta), L/2 - L*cos(theta), 0)
+    v = L*omega
+    vx = v*cos(theta)
+    vy = v*sin(theta)
+    rope.axis = r
+# 更新代表速度的箭頭位置、方向、長度
+    arrow_v.pos = ball.pos
+    arrow_vx.pos = ball.pos
+    arrow_vy.pos = ball.pos
+    arrow_v.axis = vec(vx, vy, 0)
+    arrow_vx.axis = vec(vx, 0, 0)
+    arrow_vy.axis = vec(0, vy, 0)
+# 畫出 theta-t, omega-t, alpha-t 圖
+    theta_t.plot(pos=(t, theta))
+    omega_t.plot(pos=(t, omega))
+    alpha_t.plot(pos=(t, alpha))
+# 固定時長測角度
+    now_time = time.time()
+    last_time = sampling(now_time,last_time)
+# 檢驗小球是否經過一個週期
+    omega_c = omega
+    if(omega_p > 0 and omega_c < 0):
+        i += 1
+        print(i, t)
+    omega_p = omega_c
+# 更新時間
+    t += dt
+
+print(degree_list)
---
+++ Resources/PYTHON_TOOLS/rotate_2_generate.py
@@ -0,0 +1,103 @@
+import cv2
+import numpy as np
+import os
+import math
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+filelist = os.listdir(dir_path)
+
+
+def rotate_img(img,angle):
+    
+    M = np.float32([[1, 0, 476], [0, 1, 0]])
+
+    #img = cv2.resize(img)
+    full_img = cv2.warpAffine(img, M, (1100, 800))
+
+    
+    center = (550,85)
+
+    
+    M2 = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotate_img = cv2.warpAffine(full_img, M2, (1100, 800))
+    
+    scale = 1.5
+    center = (550*scale,85*scale)
+    resize_img = cv2.resize(rotate_img,(0,0),fx=scale,fy=scale,interpolation=cv2.INTER_AREA)
+
+    # 裁切區域的 x 與 y 座標（左上角）
+    x = int(550*(scale-1))
+    y = int(85*(scale-1))
+
+    # 裁切區域的長度與寬度
+    w = 1100
+    h = 800
+
+    # 裁切圖片
+    crop_img = resize_img[y:y+h, x:x+w]
+
+    return crop_img
+
+
+if __name__ == "__main__":
+    
+    # 讀取
+    for file in filelist:
+        name, ext = os.path.splitext(file)
+
+        if ext == ".png":
+            
+            print(file)
+            filepath = dir_path + '/' + file
+            img = cv2.imread(filepath)
+
+
+            num = 1
+
+            # for angle in range(70,5,-5):
+
+            #     img_rotated = rotate_img(img, -angle)
+
+            #     if not os.path.isdir(dir_path+"/rotate/"):
+            #         os.mkdir(dir_path+"/rotate/")
+
+
+            #     newfilepath = dir_path + "/rotate/" + str(num) +".png"
+            #     cv2.imwrite(newfilepath, img_rotated)
+            #     num = num + 1
+
+            # angle = 0
+            # img_rotated = rotate_img(img, angle)
+
+            # if not os.path.isdir(dir_path+"/rotate/"):
+            #     os.mkdir(dir_path+"/rotate/")
+
+
+            # newfilepath = dir_path + "/rotate/" + str(num) +".png"
+            # cv2.imwrite(newfilepath, img_rotated)
+
+            # num = num + 1
+            
+            angles_a_round = [70, 69, 67, 64, 60, 56, 50, 44, 36, 28, 20, 15, 8, 2, -5, -14, -23, -30, -36, -42, -48, -53, -57, -61, -65, -67, -68, -68, -68, -67, -66, -63, -59, -54, -48, -42, -35, -28, -20, -12, -5, 2, 10, 17, 25, 33, 39, 46, 52, 57, 61, 64, 67, 69, 70]
+            
+            
+
+            for angle in angles_a_round:
+
+                img_rotated = rotate_img(img, angle)
+
+                if not os.path.isdir(dir_path+"/rotate/"):
+                    os.mkdir(dir_path+"/rotate/")
+
+
+                newfilepath = dir_path + "/rotate/" + str(num) +".png"
+                cv2.imwrite(newfilepath, img_rotated)
+
+                num = num + 1
+            
+
+            
+            
+            
+            
+            
